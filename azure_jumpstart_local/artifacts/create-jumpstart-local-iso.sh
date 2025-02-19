e0d79ed0e1b766bf2b41e5eea5c74a9852582cff
Ricardo Mendes <rlfmendes@hotmail.com>
2025-02-18 18:51:31 -0800

    Even more placeholder

--- azure_jumpstart_local/artifacts/create-jumpstart-local-iso.sh
+++ azure_jumpstart_local/artifacts/create-jumpstart-local-iso.sh
@@ -1,11 +1,12 @@
+#!/bin/bash
+
 echo "hello from create-jumpstart-local-iso.sh"
+
+echo "Parameter 1: $1"
+echo "Parameter 2: $2"
 
 if ! type "$xorriso" > /dev/null; then
     # install foobar here
     echo "xorriso not found. Installing."
     # sudo apt -y install xorriso
 fi
-
-
-
-
